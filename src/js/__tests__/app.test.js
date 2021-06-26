7b02cae466ef390cd11385c43f379c48d21e7072
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-06-26 15:54:59 +0300

    add tests

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,7 @@
+import demo from '../app';
+
+test('тестируем возвращенное значение из функции', () => {
+  const testValue = 'testValue';
+  const result = demo(testValue);
+  expect(result).toBe(testValue);
+})
